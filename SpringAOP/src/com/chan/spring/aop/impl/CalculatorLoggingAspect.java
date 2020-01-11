d40c8a74e2ad643ddbc66be4f2dda2116e211741
chan21252 <chanjobs21252@gmail.com>
2020-01-11 15:52:12 +0800

    AOP-后置，返回，异常通知

--- SpringAOP/src/com/chan/spring/aop/impl/CalculatorLoggingAspect.java
+++ SpringAOP/src/com/chan/spring/aop/impl/CalculatorLoggingAspect.java
@@ -1,8 +1,7 @@
 package com.chan.spring.aop.impl;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -13,9 +12,38 @@ import java.util.List;
 public class CalculatorLoggingAspect {
 
     @Before("execution(public int com.chan.spring.aop.impl.CalculatorImp.*(int, int))")
-    public void beforeMethod(JoinPoint joinPoint) {
+    public void beforeAdvice(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
         System.out.println(methodName + " begins with" + args);
+    }
+
+    /**
+     * 后置通知，在方法执行之后执行，无论是否发生异常
+     * @param joinPoint 连接点
+     */
+    @After("execution(public int com.chan.spring.aop.impl.CalculatorImp.*(..))")
+    public void afterAdvice(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println(methodName + " ends");
+    }
+
+    /**
+     * 后置返回通知，方法正常返回之后执行
+     * @param joinPoint 连接点
+     * @param result 返回值
+     */
+    @AfterReturning(value = "execution(public int com.chan.spring.aop.impl.CalculatorImp.*(..))",
+    returning = "result")
+    public void returnAdvice(JoinPoint joinPoint, Object result) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println(methodName + " ends with " + result);
+    }
+
+    @AfterThrowing(value = "execution(public int com.chan.spring.aop.impl.CalculatorImp.*(..))",
+            throwing = "ex")
+    public void throwingAdvice(JoinPoint joinPoint, Exception ex) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println(methodName + " occurs exception " + ex);
     }
 }
--- SpringAOP/src/com/chan/spring/aop/impl/Main.java
+++ SpringAOP/src/com/chan/spring/aop/impl/Main.java
@@ -8,6 +8,8 @@ public class Main {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("com/chan/spring/aop/conf/applicationContext.xml");
         Calculator calculatorImpl = (Calculator) ctx.getBean("calculatorImp");
         int result = calculatorImpl.add(2, 3);
+        int result2 = calculatorImpl.div(25, 0);
         System.out.println(result);
+        System.out.println(result2);
     }
 }
